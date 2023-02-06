4345bb8ebddaa5ccb80ff7762b6ea1a9afadb64b
= <harry940816@gmail.com>
2023-02-06 21:55:54 +0800

    add like function

--- js/shop.js
+++ js/shop.js
@@ -46,79 +46,109 @@ setInterval(() => {
 let data = {
     merches:[
         {
+            id:1,
             merchPicSrc:'pictures/sausage6.png',
             merchName:'normal sausage',
             merchPrice:180,
+            like:false,
         },
         {
+            id:2,
             merchPicSrc:'pictures/sausage1.png',
             merchName:'sweet sausage',
             merchPrice:150,
+            like:false,
         },
         {
+            id:3,
             merchPicSrc:'pictures/sausage2.png',
             merchName:'spicy sausage',
             merchPrice:180,
+            like:false,
         },
         {
+            id:4,
             merchPicSrc:'pictures/sausage3.png',
             merchName:'pepper sausage',
             merchPrice:120,
+            like:false,
         },
         {
+            id:5,
             merchPicSrc:'pictures/sausage4.png',
             merchName:'garlic sausage',
             merchPrice:130,
+            like:false,
         },
         {
+            id:6,
             merchPicSrc:'pictures/sausage5.png',
             merchName:'curry sausage',
             merchPrice:200,
+            like:false,
         },
         {
+            id:7,
             merchPicSrc:'pictures/sausage6.png',
             merchName:'wine sausage',
             merchPrice:150,
+            like:false,
         },
         {
+            id:8,
             merchPicSrc:'pictures/sausage1.png',
             merchName:'sweet sausage',
             merchPrice:185,
+            like:false,
         },
         {
+            id:9,
             merchPicSrc:'pictures/sausage2.png',
             merchName:'spicy sausage',
             merchPrice:185,
+            like:false,
         },
         {
+            id:10,
             merchPicSrc:'pictures/sausage3.png',
             merchName:'pepper sausage',
             merchPrice:199,
+            like:false,
         },
         {
+            id:11,
             merchPicSrc:'pictures/sausage1.png',
             merchName:'sweet sausage',
             merchPrice:100,
+            like:false,
         },
         {
+            id:12,
             merchPicSrc:'pictures/sausage2.png',
             merchName:'spicy sausage',
             merchPrice:170,
+            like:false,
         },
         {
+            id:13,
             merchPicSrc:'pictures/sausage3.png',
             merchName:'pepper sausage',
             merchPrice:145,
+            like:false,
         },
         {
+            id:14,
             merchPicSrc:'pictures/sausage4.png',
             merchName:'spicy sausage',
             merchPrice:185,
+            like:false,
         },
         {
+            id:15,
             merchPicSrc:'pictures/sausage5.png',
             merchName:'spicy sausage',
             merchPrice:180,
+            like:false,
         }
     ],
     showRange:9,
@@ -138,23 +168,23 @@ const vm = new Vue({
         //filter
         OnList(){
             if(this.inputSearch != ""){
-                let inputSearchMerches = this.merches.filter(item => {
+                let merchesOnList = this.merches.filter(item => {
                     let inputText = this.inputSearch.toLowerCase()
                         if(item.merchName.indexOf(inputText) != -1){
                             return item
                         }
                 })
                 if(this.filter.flavor == 'all')
-                    return this.sortByPrice(inputSearchMerches)
+                    return this.sortByPrice(merchesOnList)
                 else{
-                    return this.sortByPrice(inputSearchMerches.filter(item => {
+                    return this.sortByPrice(merchesOnList.filter(item => {
                         if(item.merchName.indexOf(this.filter.flavor) != -1)
                             return item
                     }))
                 }
             }
             else if(this.filter.flavor != "all"){
-                    return this.sortByPrice(this.merches.filter(item => {
+                    return merchesOnList = this.sortByPrice(this.merches.filter(item => {
                         if(item.merchName.indexOf(this.filter.flavor) != -1)
                             return item
                     }))
@@ -163,8 +193,15 @@ const vm = new Vue({
                 for(let i = 0;i < this.showRange;i ++){
                     this.merchesOnList.push(this.merches[i])
                 }
-                return this.sortByPrice(this.merchesOnList)
+                return merchesOnList = this.sortByPrice(this.merchesOnList)
             }
+        },
+        //be chosen and like == false => add in collection
+        collection(){
+            return this.merchesOnList.filter(item => {
+                if(item.like == true)
+                    return item
+            })
         }
     },
     methods:{
@@ -180,7 +217,6 @@ const vm = new Vue({
             location.href = `merch.html?index=${index}`;
         },
         sortByPrice(arr){
-            console.log('asdfaf');
             if(this.filter.price == 'price_highToLow')
             return arr.sort(function (first,second) {
                 return second.merchPrice - first.merchPrice
@@ -193,6 +229,22 @@ const vm = new Vue({
             else {
                 return arr              //default
             }
+        },
+        changeLike(index){
+            let boolean = (merchesOnList[index].like ? false : true) 
+            // if(merchesOnList[index].like == false){
+            //     merchesOnList[index].like = true
+            // }
+            merchesOnList[index].like = boolean
+            //需優化
+            let legnth = this.merches.length
+            for(let i = 0;i < length;i ++){
+                if(this.merches[i].id == merchesOnList[index].id){
+                    this.merches[i].like = boolean
+                    break
+                }
+            }
+
         }
     }
 })
@@ -226,16 +278,6 @@ function changeHeart(id) {
         }
     }
 }   
-//openCollection
-function openCollection() {
-    let collection = document.getElementById('collection');
-    collection.innerHTML = '';
-    let length = collectionId.length;
-    for(let i = 0;i < length;i ++){
-        let innerText = document.getElementById("collectionInnerText"+collectionId[i].match(/\d+/g)).innerHTML;
-        collection.innerHTML += innerText;
-    }
-}
 
                
 
